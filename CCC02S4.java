ac1036fe3755dedd5ec5fbbab005bc27c56338fd
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-17 23:10:36 -0500

    Update CCC02S4.java

--- CCC02S4.java
+++ CCC02S4.java
@@ -1,59 +1,92 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
 /**
  * CCC '02 S4 - Bridge Crossing
- * Question type: Dynamic Programming
- * 5/5 on DMOJ
- * @author Milliken high school -> http://mmhs.ca/ccc/index.htm
+ * Question URL: Dynamic Programming
+ * 50/50 on DMOJ
+ * Question URL: https://dmoj.ca/problem/ccc02s4
+ * @author Tommy Pang
  */
 public class CCC02S4 {
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    public static void main(String[] args) throws IOException{
-        int M = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
-        String [] name = new String[N];
-        int [] time = new int[N];
-        for (int i = 0; i < N; i++) {
-            name[i] = br.readLine();
-            time[i] = Integer.parseInt(br.readLine());
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    static int M, Q, mod = (int) 1e9+7;
+    static int [] dp, path;
+    public static void main(String[] args) throws IOException {
+        M = readInt(); Q = readInt();
+        dp = new int[Q+1]; // dp[i] -> for the first i person, what's the min time required
+        path = new int[Q+1]; // the index of the last person in the previous group
+        List<person> queue = new ArrayList<>();
+        queue.add(null);
+        for (int i = 0; i < Q; i++) {
+            queue.add(new person(readLine(), readInt()));
         }
-        int [] best = new int[N+1];
-        int [] group = new int[N+1];
-        for (int i = 0; i < N+1; i++) {
-            best[i] = 1000000;
-            group[i] = -1;
-        }
-        best[0] = 0;
-        group[0] = 0;
-        for (int i = 0; i < N+1; i++) {
-            int cur = 0;
-            for (int j = 1; j <= M && i+j - 1 < N; j++) {
-                cur = Math.max(cur, time[i + j - 1]);
-                if (best [i] + cur < best [i + j]) {
-                    best [i + j] = best [i] + cur;
-                    group [i + j] = j;
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        path[0] = -1;
+        for (int i = 1; i <= Q; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (i-j>=0) {
+                    int l = i-j;
+                    int max = getMax(queue, l, i);
+                    if (dp[l]+max<dp[i]) {
+                        path[i] = l;
+                        dp[i] = dp[l]+max;
+                    }
                 }
             }
         }
-        System.out.println("Total Time: " + best [N]);
-        int [] lines = new int [N + 1];
-        int k = N;
-        int x = 0;
-        while (group [k] != 0) {
-            lines [x++] = group [k];
-            k = k - group [k];
+        int cur = Q;
+        List<Integer> order = new ArrayList<>();
+        order.add(cur);
+        while (path[cur]!=-1) {
+            order.add(path[cur]);
+            cur = path[cur];
         }
-        int z = 0;
-        for (int i = x - 1 ; i >= 0 ; i--) {
-            for (int j = 0 ; j < lines [i] ; j++) {
-                System.out.print(name[z++] + " ");
+        order.remove(order.size()-1);
+        Collections.reverse(order);
+        int lst = 1;
+        System.out.println("Total Time: " + dp[Q]);
+        for (int i = 0; i < order.size(); i++) {
+            for (int j = lst; j <= order.get(i); j++) {
+                System.out.print(queue.get(j).name + " ");
             }
+            lst = order.get(i)+1;
             System.out.println();
         }
-
+    }
+    public static int getMax(List<person> q, int l, int r) {
+        int max = 0;
+        for (int i = l+1; i <= r; i++) {
+            max = Math.max(max, q.get(i).time);
+        }
+        return max;
+    }
+    public static class person {
+        String name;
+        int time;
+        person(String name, int time) { this.name = name; this.time = time; }
+    }
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
     }
 }
