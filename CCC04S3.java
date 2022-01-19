edfaeb96f764f96eab199fedab1d25ea155cf178
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-19 15:29:11 -0500

    Update CCC04S3.java

--- CCC04S3.java
+++ CCC04S3.java
@@ -5,101 +5,105 @@ import java.util.stream.Stream;
 /**
  * CCC '04 S3 - Spreadsheet
  * Question type: Graph Theory
- * 3/5 on DMOJ, case 3, 4 WA
+ * 5/5 on DMOJ
+ * Question URL: https://dmoj.ca/problem/ccc04s3
  * @author Tommy Pang
  */
 public class CCC04S3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
-    static Map<String, Integer> map = Stream.of(new Object[][]{
-            {"A", 0},
-            {"B", 1},
-            {"C", 2},
-            {"D", 3},
-            {"E", 4},
-            {"F", 5},
-            {"G", 6},
-            {"H", 7},
-            {"I", 8},
-            {"J", 9},
-    }).collect(Collectors.toMap(data -> (String) data[0], data -> (Integer) data[1]));
-    static String[][] elements = new String[10][9];
-    static boolean[][] defined = new boolean[10][9];
-    static int[][] result = new int[10][9];
-
-    static {
-        for (int[] arr1 : result)
-            Arrays.fill(arr1, -1);
-    }
-
+    static int mod = (int) 1e9+7;
+    static String[][] grid = new String[10][10];
+    static boolean[][] vis = new boolean[10][10];
     public static void main(String[] args) throws IOException {
-        readInput();
-        traverseArray();
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (result[i][j] != -1) {
-                    System.out.print(elements[i][j] + " ");
-                } else {
-                    System.out.print("* ");
-                }
+            String [] cols = readLine().split(" ");
+            for (int j = 1; j <= 9; j++) {
+                grid[i][j] = cols[j-1];
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j <= 9; j++) {
+                vis = new boolean[10][10];
+                dfs(i, j);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
     }
-
-    private static void readInput() throws IOException {
-        for (int i = 0; i < 10; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 9; j++) {
-                String s = st.nextToken();
-                elements[i][j] = s;
-                try {
-                    int num = Integer.parseInt(s);
-                    if (!(num < 0)) {
-                        result[i][j] = num;
-                    } else {
-                        throw new IOException();
-                    }
-                } catch (NumberFormatException e) {
-                }
+    public static int dfs(int r, int c) {
+        if (isNumeric(grid[r][c])) return Integer.parseInt(grid[r][c]);
+        if (vis[r][c] || grid[r][c].equals("*")) return -1;
+        vis[r][c] = true;
+        String [] depend = grid[r][c].split("\\+");
+        int sum = 0;
+        for (int i = 0; i < depend.length; i++) {
+            int ret = dfs(depend[i].charAt(0)-'A', depend[i].charAt(1)-'0');
+            if (ret==-1) {
+                grid[r][c] = "*";
+                return -1;
             }
+            else sum+=ret;
         }
+        grid[r][c] = String.valueOf(sum);
+        return sum;
+    }
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        }
+        catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
-    private static void traverseArray() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (result[i][j] == -1) {
-                    List<String> pre = new ArrayList<>();
-                    pre.add(elements[i][j]);
-                    int temp = calculateCell(elements[i][j], pre);
-                    if (temp != 0) {
-                        result[i][j] = temp;
-                        elements[i][j] = String.valueOf(temp);
-                    }
-                }
-            }
-        }
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
     }
-
-    private static int calculateCell(String toCalculate, List pre) {
-        String[] temp = toCalculate.split("\\+");
-        int val = 0;
-        for (int k = 0; k < temp.length; k++) {
-            String letter = String.valueOf(temp[k].charAt(0));
-            String num = String.valueOf(temp[k].charAt(1));
-            int v;
-            if (result[map.get(letter)][Integer.parseInt(num) - 1] != -1) {
-                val = val + Integer.parseInt(elements[map.get(letter)][Integer.parseInt(num)-1]);
-            } else {
-                if (pre.contains(elements[map.get(letter)][Integer.parseInt(num)-1])) return 0;
-                else {
-                    pre.add(elements[map.get(letter)][Integer.parseInt(num)-1]);
-                    v = calculateCell(elements[map.get(letter)][Integer.parseInt(num)-1], pre);
-                }
-                val = (v == 0) ? 0 : val + v;
-            }
-        }
-        return val;
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    static int readLongLineInt() throws IOException{
+        int x = 0, c;
+        while((c = br.read()) != ' ' && c != '\n')
+            x = x * 10 + (c - '0');
+        return x;
+    }
+    static long pow (long x, long exp){
+        if (exp==0) return 1;
+        long t = pow(x, exp/2);
+        t = t*t %mod;
+        if (exp%2 == 0) return t;
+        return t*x%mod;
+    }
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
