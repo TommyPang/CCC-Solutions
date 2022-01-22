854897f804218dc258356df13521eb0a4db4c321
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-22 18:18:20 -0500

    Update CCC08S5.java

--- CCC08S5.java
+++ CCC08S5.java
@@ -1,91 +1,131 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 /**
  * CCC '08 S5 - Nukit
  * Question type: Dynamic Programming, Game Theory
  * 7/7 on DMOJ
- * @author Tommy Pang, idea taken from http://mmhs.ca/ccc/index.htm
+ * Question URL: https://dmoj.ca/problem/ccc08s5
+ * @author Tommy Pang
  */
 public class CCC08S5 {
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static String [] dp;
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    static int mod = (int) 1e9+7, T, A, B, C, D;
+    static int [][][][] dp;
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
-            dp = new String[1000000];
-            Arrays.fill(dp, "?");
-            if (winning(a, b, c, d)) System.out.println("Patrick");
+        T = readInt();
+        for (int i = 0; i < T; i++) {
+            dp = new int[31][31][31][31];
+            init();
+            A = readInt(); B = readInt(); C = readInt(); D = readInt();
+            if (win(A, B, C, D)) System.out.println("Patrick");
             else System.out.println("Roland");
         }
     }
-    static boolean winning(int A, int B, int C, int D){
-        int x = 27000*A+900*B+30*C+D;
-        if (!dp[x].equals("?")) return dp[x].equals("w");
-        // 1
-        if (A-2>=0 && B-1>=0 && D-2>=0 && losing(A-2, B-1, C, D-2)) {
-            dp[x] = "w";
-            return true;
-        }
-        //2
-        if (A-1>=0 && B-1>=0 && C-1>=0 && D-1>=0 && losing(A-1, B-1, C-1, D-1)) {
-            dp[x] = "w";
-            return true;
-        }
-        //3
-        if (C-2>=0 && D-1>=0 && losing(A, B, C-2, D-1)) {
-            dp[x] = "w";
-            return true;
-        }
-        //4
-        if (B-3>=0 && losing(A, B-3, C, D)) {
-            dp[x] = "w";
-            return true;
-        }
-        //5
-        if (A-1>=0 && D-1>=0 && losing(A-1, B, C, D-1)) {
-            dp[x] = "w";
-            return true;
-        }
-        dp[x] = "l";
-        return false;
-    }
-    static boolean losing(int A, int B, int C, int D){
-        int x = 27000*A+900*B+30*C+D;
-        if (!dp[x].equals("?")) return dp[x].equals("l");
+    public static boolean win(int A, int B, int C, int D) {
+        if (dp[A][B][C][D]!=-1) return dp[A][B][C][D] == 1;
         if (!canMove(A, B, C, D)) {
-            dp[x] = "l";
-            return true;
+            dp[A][B][C][D] = 0;
+            return false; // loses
         }
-        else {
-            boolean temp = true;
-            if (A-2>=0 && B-1>=0 && D-2>=0) temp = temp&&winning(A-2, B-1, C, D-2);
-            if (A-1>=0 && B-1>=0 && C-1>=0 && D-1>=0) temp = temp&&winning(A-1, B-1, C-1, D-1);
-            if (C-2>=0 && D-1>=0) temp = temp&&winning(A, B, C-2, D-1);
-            if (B-3>=0) temp = temp&&winning(A, B-3, C, D);
-            if (A-1>=0 && D-1>=0) temp = temp&&winning(A-1, B, C, D-1);
-            if (temp){
-                dp[x] = "l";
+        // 1.AABDD
+        if (A>=2 && B>=1 && D>=2) {
+            if (!win(A-2, B-1, C, D-2)) {
+                dp[A][B][C][D] = 1;
                 return true;
             }
-            else {
-                dp[x] = "w";
-                return false;
+        }
+        // 2.ABCD
+        if (A>=1 && B>=1 && C>=1 && D>=1) {
+            if (!win(A-1, B-1, C-1, D-1)) {
+                dp[A][B][C][D] = 1;
+                return true;
+            }
+        }
+        // 3.CCD
+        if (C>=2 && D>=1) {
+            if (!win(A, B, C-2, D-1)) {
+                dp[A][B][C][D] = 1;
+                return true;
+            }
+        }
+        // 4.BBB
+        if (B>=3) {
+            if (!win(A, B-3, C, D)) {
+                dp[A][B][C][D] = 1;
+                return true;
+            }
+        }
+        // 5.AD
+        if (A>=1 && D>=1) {
+            if (!win(A-1, B, C, D-1)) {
+                dp[A][B][C][D] = 1;
+                return true;
+            }
+        }
+        dp[A][B][C][D] = 0;
+        return false;
+    }
+    public static boolean canMove(int A, int B, int C, int D) {
+        boolean ret = A>=2 && B>=1 && D>=2;
+        ret |= A>=1 && B>=1 && C>=1 && D>=1;
+        ret |= C>=2 && D>=1;
+        ret |= B>=3;
+        ret |= A>=1 && D>=1;
+        return ret;
+    }
+    public static void init() {
+        for (int i = 0; i < 31; i++) {
+            for (int j = 0; j < 31; j++) {
+                for (int k = 0; k < 31; k++) {
+                    for (int l = 0; l < 31; l++) {
+                        dp[i][j][k][l] = -1;
+                    }
+                }
             }
         }
     }
-    static boolean canMove(int A, int B, int C, int D){
-        if (A-2>=0 && B-1>=0 && D-2>=0) return true;
-        if (A-1>=0 && B-1>=0 && C-1>=0 && D-1>=0) return true;
-        if (C-2>=0 && D-1>=0) return true;
-        if (B-3>=0) return true;
-        if (A-1>=0 && D-1>=0) return true;
-        return false;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    static int readLongLineInt() throws IOException{
+        int x = 0, c;
+        while((c = br.read()) != ' ' && c != '\n')
+            x = x * 10 + (c - '0');
+        return x;
+    }
+    static long pow (long x, long exp){
+        if (exp==0) return 1;
+        long t = pow(x, exp/2);
+        t = t*t %mod;
+        if (exp%2 == 0) return t;
+        return t*x%mod;
+    }
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
