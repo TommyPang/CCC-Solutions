e98c2aee24b1531638ec2acdaac3e3c30ab691e8
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-24 12:02:24 -0500

    Update CCC09S2.java

--- CCC09S2.java
+++ CCC09S2.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -10,28 +8,70 @@ import java.util.StringTokenizer;
  * 5/5 on DMOJ
  * @author Tommy Pang
  */
-public class CCC09S2_BitOperation {
+public class CCC09S2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
-    static int [] row;
-    static int R, L;
-    public static void main(String[] args) throws IOException{
-        R = Integer.parseInt(br.readLine());
-        row = new int[R];
-        L = Integer.parseInt(br.readLine());
-        for(int i=0; i<R; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<L; j++) {
-                row[i] = row[i]<<1 | Integer.parseInt(st.nextToken());
+    static int mod = (int) 1e9+7, R, L;
+    public static void main(String[] args) throws IOException {
+        R = readInt(); L = readInt();
+        Set<Integer> cur = new HashSet<>();
+        Set<Integer> nxt = new HashSet<>();
+        int cur_mask;
+        for (int i = 0; i < R; i++) {
+            cur_mask = 0;
+            for (int j = 0; j < L; j++) {
+                cur_mask = (cur_mask<<1) | readInt();
             }
+            nxt.add(cur_mask);
+            for (int nxt_mask : cur) {
+                nxt.add(cur_mask^nxt_mask);
+            }
+            cur = nxt;
+            nxt = new HashSet<>();
         }
-        Set<Integer> pattern[] = new HashSet[R];
-        for(int i=0; i<R; i++) pattern[i] = new HashSet();
-        pattern[0].add(row[0]);
-        for(int i=1; i<R; i++) {
-            pattern[i].add(row[i]);
-            for(int x: pattern[i-1]) pattern[i].add(row[i] ^ x);
-        }
-        System.out.println(pattern[R-1].size());
+        System.out.println(cur.size());
     }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    static int readLongLineInt() throws IOException{
+        int x = 0, c;
+        while((c = br.read()) != ' ' && c != '\n')
+            x = x * 10 + (c - '0');
+        return x;
+    }
+    static long pow (long x, long exp){
+        if (exp==0) return 1;
+        long t = pow(x, exp/2);
+        t = t*t %mod;
+        if (exp%2 == 0) return t;
+        return t*x%mod;
+    }
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
 }
