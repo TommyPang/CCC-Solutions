0ad36d44384695127ffaa317134fa1c60fb8aae5
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-27 23:24:14 -0500

    Update CCC13S4.java

--- CCC13S4.java
+++ CCC13S4.java
@@ -1,63 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 /**
  * CCC '13 S4 - Who is Taller?
- * Question type: Graph Theory
+ * Question URL: Graph Theory
  * 300/300 on DMOJ
  * Question URL: https://dmoj.ca/problem/ccc13s4
  * @author Tommy Pang
  */
 public class CCC13S4 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        List<Integer> adj [] = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
+    static int mod = (int) 1e9+7, n, m;
+    static List<Integer> [] adj;
+    static boolean[] vis;
+    public static void main(String[] args) throws IOException {
+        n = readInt(); m = readInt();
+        adj = new List[n+1]; vis = new boolean[n+1];
+        for (int i = 1; i <= n; i++) {
             adj[i] = new ArrayList<>();
         }
-        int M = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
-            adj[x].add(y);
+        for (int i = 0; i < m; i++) {
+            adj[readInt()].add(readInt());
         }
-        st = new StringTokenizer(br.readLine());
-        int p = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
-        boolean [] vis = new boolean[N+1];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(p); vis[p] = true;
-        while (!queue.isEmpty()){
+        int p = readInt(), q = readInt();
+        if (BFS(p, q)) System.out.println("yes");
+        else if (BFS(q, p)) System.out.println("no");
+        else System.out.println("unknown");
+    }
+    static boolean BFS(int p, int q) {
+        Queue<Integer> queue = new LinkedList<>(); vis = new boolean[n+1];
+        queue.add(p);
+        while (!queue.isEmpty()) {
             int cur = queue.poll();
             for (int nxt : adj[cur]) {
                 if (!vis[nxt]) {
-                    queue.add(nxt);
                     vis[nxt] = true;
+                    queue.add(nxt);
                 }
             }
         }
-        if (vis[q]) {
-            System.out.println("yes");
-        }
-        else {
-            vis = new boolean[N+1];
-            queue = new LinkedList<>();
-            queue.add(q); vis[q] = true;
-            while (!queue.isEmpty()){
-                int cur = queue.poll();
-                for (int nxt : adj[cur]) {
-                    if (!vis[nxt]) {
-                        queue.add(nxt);
-                        vis[nxt] = true;
-                    }
-                }
-            }
-            if (vis[p]) System.out.println("no");
-            else System.out.println("unknown");
-        }
+        return vis[q];
     }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
 }
