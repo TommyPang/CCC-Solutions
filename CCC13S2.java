94892cc245ee0ba2a55080ef53b6abc672db1f40
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-01-27 22:05:04 -0500

    Update CCC13S2.java

--- CCC13S2.java
+++ CCC13S2.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 /**
  * CCC '13 S2 - Bridge Transport
@@ -11,33 +9,52 @@ import java.util.StringTokenizer;
  */
 public class CCC13S2 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
-    static int cnt = 0;
-    public static void main(String[] args) throws IOException{
-        int W = Integer.parseInt(br.readLine());
-        int N = Integer.parseInt(br.readLine());
-        int [] weights = new int[N+1];
-        for (int i = 1; i <= N; i++) {
-            weights[i] = Integer.parseInt(br.readLine());
+    static int mod = (int) 1e9+7, T, n;
+    public static void main(String[] args) throws IOException {
+        T = readInt(); n = readInt();
+        int [] cars = new int[n];
+        for (int i = 0; i < n; i++) {
+            cars[i] = readInt();
         }
-        boolean [] vis = new boolean[N+1];
-        int total = 0;
-        for (int i = 1; i <= N-4+1; i++) {
-            for (int j = i; j < i+4; j++) {
-                if (!vis[j]) {
-                    total += weights[j];
-                    if (total <= W) {
-                        cnt += 1;
-                        vis[j] = true;
-                    }
-                    else {
-                        System.out.println(cnt);
-                        return;
-                    }
-                }
+        int sum = 0;
+        for (int i = 0; i < Math.min(4, n); i++) {
+            sum+=cars[i];
+            if (sum>T) {
+                System.out.println(i);
+                return;
             }
-            total-=weights[i];
         }
-        System.out.println(cnt);
+        for (int i = 4; i < n; i++) {
+            sum-=cars[i-4];
+            sum+=cars[i];
+            if (sum>T) {
+                System.out.println(i);
+                return;
+            }
+        }
+        System.out.println(n);
     }
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+
 }
