c0408a50cdda2f7700a1bb7f5e337c1eb773b688
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-02-04 23:21:33 -0500

    Update CCC16S3.java

--- CCC16S3.java
+++ CCC16S3.java
@@ -1,109 +1,125 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 /**
  * CCC '16 S3 - Phonomenal Reviews
- * Question type: Graph Theory
- * 13/15 on DMOJ, Batch #7 TLE
+ * Question URL: Graph Theory
+ * 15/15 on DMOJ
  * Question URL: https://dmoj.ca/problem/ccc16s3
  * @author Tommy Pang
  */
 public class CCC16S3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
     static StringTokenizer st;
-    static boolean [] notUseful;
-    static int [] in;
-    static List<Integer> restaurant = new ArrayList<>();
-    static List<Integer>[] adj;
-    static int N, M;
-    static Queue<Integer> queue = new LinkedList<>();
-    static int [] dis;
-    static boolean [] vis;
-    public static void main(String[] args) throws IOException{
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        dis = new int[N]; vis = new boolean[N];
-        M = Integer.parseInt(st.nextToken());
-        adj = new ArrayList[N];
-        in = new int[N];
-        notUseful = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            adj[i] = new ArrayList();
+    static int mod = (int) 1e9+7, n, m, MM = (int) 1e5+5;
+    static List<Integer> [] adj = new ArrayList[MM];
+    static boolean[] isRest = new boolean[MM];
+    static int max, u, total_node;
+    static int[] dis = new int[MM];
+    static boolean[] vis = new boolean[MM];
+    static {
+        for (int i = 0; i < MM; i++) {
+            adj[i] = new ArrayList<>();
         }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            restaurant.add(Integer.parseInt(st.nextToken()));
-        }
-        for (int i = 0; i < N-1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            adj[start].add(end); adj[end].add(start);
-            in[start]+=1; in[end]+=1;
-        }
-        cutEdge();
-        int cutted = 0;
-        int point = 0;
-        for (int i = 0; i < N; i++) {
-            if (notUseful[i]) cutted+=1;
-            else point = i;
-        }
-        int E = N - cutted - 1;
-        int diameter = FindDiameter(point);
-        System.out.println(2*E - diameter);
     }
-    static void cutEdge(){
+    public static void main(String[] args) throws IOException {
+        input(); total_node = n;
+        dfs(u, -1);
+        System.out.println((total_node-1)*2-FindDiameter(u));
+    }
+    static int FindDiameter(int i) {
+        dis = new int[MM];
+        vis = new boolean[MM];
+        max = 1; u = -1;
+        BFS(i);
+        dis = new int[MM];
+        vis = new boolean[MM];
+        max = 1;
+        BFS(u);
+        return max;
+    }
+
+    static void BFS(int i){
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            if (in[i] == 1 && !restaurant.contains(i)){
-                notUseful[i] = true;
-                queue.add(i);
-            }
-        }
+        queue.add(i); vis[i] = true; dis[i] = 0; u = i; max = 0;
         while (!queue.isEmpty()){
             int cur = queue.poll();
-            for (int i = 0; i < N; i++) {
-                if (adj[i].contains(cur)){
-                    in[i]-=1;
-                    if (in[i]==1 && !restaurant.contains(i)) {
-                        queue.add(i);
-                        notUseful[i] = true;
+            for (int nxt : adj[cur]) {
+                if (!vis[nxt] && isRest[nxt]) {
+                    queue.add(nxt);
+                    vis[nxt] = true;
+                    dis[nxt] = dis[cur]+1;
+                    if (dis[nxt]>max) {
+                        u = nxt;
+                        max = dis[nxt];
                     }
                 }
             }
         }
     }
-    static int FindDiameter(int i){
-        BFS(i);
-        int max = 0, idx = -1;
-        for (int j = 0; j < N; j++) {
-            if (max<dis[j]) {
-                max = dis[j];
-                idx = j;
+
+    static boolean dfs(int v, int pa) {
+        for (int nxt : adj[v]) {
+            if (nxt!=pa) {
+                isRest[v] |= dfs(nxt, v);
             }
         }
-        Arrays.fill(dis, 0);
-        Arrays.fill(vis, false);
-        BFS(idx); vis[idx] = true;
-        max = 0;
-        for (int j = 0; j < N; j++) {
-            if (max<dis[j]) {
-                max = dis[j];
-            }
-        }
-        return max;
+        if (!isRest[v]) total_node--;
+        return isRest[v];
     }
-    static void BFS(int i){
-        queue.add(i); vis[i] = true; dis[i] = 0;
-        while (!queue.isEmpty()){
-            int cur = queue.poll();
-            for (int nxt : adj[cur]) {
-                if (!vis[nxt] && !notUseful[nxt]){
-                    queue.add(nxt); vis[nxt] = true;
-                    dis[nxt] = dis[cur] + 1;
-                }
-            }
+
+    static void input() throws IOException {
+        n = readInt(); m = readInt();
+        for (int i = 0; i < m; i++) {
+            u = readInt();
+            isRest[u] = true;
         }
+        for (int i = 0; i < n-1; i++) {
+            int a = readInt(), b = readInt();
+            adj[a].add(b); adj[b].add(a);
+        }
+
+
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
+    }
+    static int readLongLineInt() throws IOException{
+        int x = 0, c;
+        while((c = br.read()) != ' ' && c != '\n')
+            x = x * 10 + (c - '0');
+        return x;
+    }
+    static long pow (long x, long exp){
+        if (exp==0) return 1;
+        long t = pow(x, exp/2);
+        t = t*t %mod;
+        if (exp%2 == 0) return t;
+        return t*x%mod;
+    }
+    static long lcm(long a, long b) {
+        return (a / gcd(a, b)) * b;
+    }
+    static long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 }
