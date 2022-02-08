b9ef2155662c1d923a5f9c416e21c2d68d8d3279
Tommy Pang <63743103+TommyPang@users.noreply.github.com>
2022-02-07 23:54:54 -0500

    Update CCC19S3.java

--- CCC19S3.java
+++ CCC19S3.java
@@ -1,44 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 /**
  * CCC '19 S3 - Arithmetic Square
  * Question type: Implementation
- * 4/16 on DMOJ, WA on Batch 2-6
+ * 11/16 on DMOJ, WA on Batch 4-6
  * Question URL: https://dmoj.ca/problem/ccc19s3
  * @author Tommy Pang
  */
+
 public class CCC19S3 {
-    static StringTokenizer st;
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static String[][] grid = new String[3][3];
-    static int max = 0;
-    static List<Integer> filledR = new ArrayList<>(), toFillR = new ArrayList<>();
-    static List<Integer> filledC = new ArrayList<>(), toFillC = new ArrayList<>();
-    public static void main(String[] args) throws IOException{
+    static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    static StringTokenizer st;
+    static int mod = (int) 1e9+7;
+    static int[][] grid = new int[3][3];
+    public static void main(String[] args) throws IOException {
         for (int i = 0; i < 3; i++) {
-            st = new StringTokenizer(br.readLine());
+            String[] lines = readLine().split(" ");
             for (int j = 0; j < 3; j++) {
-                String v = st.nextToken();
-                grid[i][j] = v;
-                if (!(v.equals("X"))) max = Math.max(Integer.parseInt(v), max);
+                grid[i][j] = lines[j].equals("X") ? Integer.MIN_VALUE : Integer.parseInt(lines[j]);
             }
         }
-        for (int i = 0; i < 3; i++) {
-            fillingRow();
-            fillingCol();
-        }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (grid[i][j].equals("X")) {
-                    toFillR.add(i);
-                    toFillC.add(j);
-                }
-            }
-        }
+        solve();
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 System.out.print(grid[i][j] + " ");
@@ -46,56 +31,103 @@ public class CCC19S3 {
             System.out.println();
         }
     }
-
-    static void fillingRow(){
+    public static boolean solve() {
+        int[][] grid2 = copy();
+        easyFill();
+        if (done()) {
+            return true;
+        }
+        loop:
         for (int i = 0; i < 3; i++) {
-            int cnt = 0;
             for (int j = 0; j < 3; j++) {
-                if (!grid[i][j].equals("X")) cnt+=1;
+                if (grid[i][j]!=Integer.MIN_VALUE) continue;
+                for (int k = -30; k <= 30; k++) { // when input is [-10, 10], we have minimum of -30 and maximum of 30
+                    grid[i][j] = k;
+                    boolean res = solve();
+                    if (res) return true;
+                }
+                break loop;
             }
-            switch (cnt){
-                case 3:
-                    continue;
-                case 2:
-                    if (grid[i][0].equals("X")){
-                        grid[i][0] = String.valueOf(Integer.parseInt(grid[i][1]) - (Integer.parseInt(grid[i][2]) - Integer.parseInt(grid[i][1])));
-                        filledR.add(i); filledC.add(0);
-                    }
-                    else if (grid[i][1].equals("X")){
-                        grid[i][1] = String.valueOf((Integer.parseInt(grid[i][0]) + Integer.parseInt(grid[i][2]))/2);
-                        filledR.add(i); filledC.add(1);
-                    }
-                    else {
-                        grid[i][2] = String.valueOf(Integer.parseInt(grid[i][1]) + (Integer.parseInt(grid[i][1]) - (Integer.parseInt(grid[i][0]))));
-                        filledR.add(i); filledC.add(2);
-                    }
-                    break;
+        }
+        grid = grid2;
+        return false;
+    }
+    public static int[][] copy() {
+        int[][] tmp = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tmp[i][j] = grid[i][j];
+            }
+        }
+        return tmp;
+    }
+
+    public static void easyFill() {
+        // fill rows
+        for (int i = 0; i < 3; i++) {
+            if (grid[i][0] == Integer.MIN_VALUE && grid[i][1] != Integer.MIN_VALUE && grid[i][2] != Integer.MIN_VALUE) {
+                grid[i][0] = grid[i][1] - (grid[i][2] - grid[i][1]);
+            }
+            if (grid[i][1] == Integer.MIN_VALUE && grid[i][0] != Integer.MIN_VALUE && grid[i][2] != Integer.MIN_VALUE) {
+                grid[i][1] = (grid[i][0] + grid[i][2]) / 2;
+            }
+            if (grid[i][2] == Integer.MIN_VALUE && grid[i][0] != Integer.MIN_VALUE && grid[i][1] != Integer.MIN_VALUE) {
+                grid[i][2] = grid[i][1] + (grid[i][1] - grid[i][0]);
+            }
+        }
+        // cols
+        for (int i = 0; i < 3; i++) {
+            if (grid[0][i] == Integer.MIN_VALUE && grid[1][i] != Integer.MIN_VALUE && grid[2][i] != Integer.MIN_VALUE) {
+                grid[0][i] = grid[1][i] - (grid[2][i] - grid[1][i]);
+            }
+            if (grid[1][i] == Integer.MIN_VALUE && grid[0][i] != Integer.MIN_VALUE && grid[2][i] != Integer.MIN_VALUE) {
+                grid[1][i] = (grid[0][i] + grid[2][i]) / 2;
+            }
+            if (grid[2][i] == Integer.MIN_VALUE && grid[0][i] != Integer.MIN_VALUE && grid[1][i] != Integer.MIN_VALUE) {
+                grid[2][i] = grid[1][i] + (grid[1][i] - grid[0][i]);
             }
         }
     }
-    static void fillingCol() {
+    public static boolean done() {
+        int ret = 0;
         for (int i = 0; i < 3; i++) {
-            int cnt = 0;
             for (int j = 0; j < 3; j++) {
-                if (!grid[j][i].equals("X")) cnt += 1;
-            }
-            switch (cnt) {
-                case 3:
-                    continue;
-                case 2:
-                    if (grid[0][i].equals("X")) {
-                        grid[0][i] = String.valueOf(Integer.parseInt(grid[1][i]) - (Integer.parseInt(grid[2][i]) - Integer.parseInt(grid[1][i])));
-                        filledR.add(0); filledC.add(i);
-                    } else if (grid[1][i].equals("X")) {
-                        grid[1][i] = String.valueOf((Integer.parseInt(grid[0][i]) + Integer.parseInt(grid[2][i])) / 2);
-                        filledR.add(1); filledC.add(i);
-                    } else {
-                        grid[2][i] = String.valueOf(Integer.parseInt(grid[1][i]) + (Integer.parseInt(grid[1][i]) - (Integer.parseInt(grid[0][i]))));
-                        filledR.add(2); filledC.add(i);
-                    }
-                    break;
+                if (grid[i][j]!=Integer.MIN_VALUE) ret++;
             }
         }
+        if (ret<9) return false;
+        if (grid[2][0]-grid[1][0]!=grid[1][0]-grid[0][0]) return false; // c1
+        if (grid[2][1]-grid[1][1]!=grid[1][1]-grid[0][1]) return false; // c2
+        if (grid[2][2]-grid[1][2]!=grid[1][2]-grid[0][2]) return false; // c3
+
+        if (grid[0][2]-grid[0][1]!=grid[0][1]-grid[0][0]) return false; // r1
+        if (grid[1][2]-grid[1][1]!=grid[1][1]-grid[1][0]) return false; // r2
+        if (grid[2][2]-grid[2][1]!=grid[2][1]-grid[2][0]) return false; // r3
+        return true;
+    }
+
+
+
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+    static String readLine() throws IOException {
+        return br.readLine().trim();
     }
 
 }
